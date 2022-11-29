bdf75298d1d80d0a69c32d5442178dc128d29d28
pjmorales27 <105305127+pjmorales27@users.noreply.github.com>
2022-11-28 21:09:11 -0500

    Add files via upload

---
+++ Rundown.sql
@@ -0,0 +1,28 @@
+Select Field_List.Field_ID, Field_List.acres, Linemen.Field_ID, Linemen.Worker_name 
+from Field_List 
+left join Linemen on 
+Field_List.Field_ID = Linemen.Field_ID;
+
+Select Field_List.Field_ID, Field_List.acres, Irrigator.Field_ID, Irrigator.Worker_name 
+from Field_List 
+left join Irrigator on 
+Field_List.Field_ID = Irrigator.Field_ID;
+
+Select Field_List.Field_ID, Field_List.acres, Tractor_Drivers.Field_ID, Tractor_Drivers.Worker_name 
+from Field_List 
+left join Tractor_Drivers on 
+Field_List.Field_ID = Tractor_Drivers.Field_ID;
+
+Select Field_List.Field_ID, Field_List.acres, Field_List.Crop_ID, Crops.Crop_ID, Crops.Crop_name
+from Field_List 
+left join Crops on 
+Field_List.Crop_ID = Crops.Crop_ID;
+
+Select Tractor_Drivers.Equipment_num,Tractor_Drivers.Worker_name, Tractors.Tractor_num, Tractors.Tractor_des, Tractors.Equipment_num  
+from  Tractor_Drivers
+left join Tractors on 
+Tractor_Drivers.Tractor_num = Tractors.Tractor_num;
+
+
+
+
