6905897f1fab969d6695e14f7d8e405634a9c4cb
PJ Morales <105305127+pjmorales27@users.noreply.github.com>
2023-03-30 15:31:57 -0400

    Add files via upload

--- calls.php
+++ calls.php
@@ -10,18 +10,45 @@ $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
 
 
 <?php
-if(isset($_POST['id']) && $_POST['id'] === 'field200') {
-    $sql = "call Get_Linemen(1);";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
+if(isset($_POST['id']) && $_POST['id'] === 'field200') { ?>
+    
 
-    if ($resultCheck > 0){
-        while ($row = mysqli_fetch_assoc($result)){
-            echo $row['Worker_name'] . "<br>";
-        }
-    }
-}
+    <table class = "table">
+        <thead>
+            <tr>
+                <th scope="col"> ID </th>
+                <th scope="col"> Linemen Name </th>
+                <th scope="col"> Task ID </th>
+                <th scope="col"> Field ID </th>
+            </tr>
+        </thead>
+            <tbody>
+                <?php
+                    $sql = "call Get_Linemen(1);";
+                    $result = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($result);
+                    if($result){
+                        while ($row = mysqli_fetch_assoc($result)){
+                            $id = $row['Linemen_ID'];
+                            $name = $row['Worker_name'];
+                            $task = $row['Task_ID'];
+                            $fieldid = $row['Field_ID'];
 
+                            echo '<tr>
+                            <th scope= "row">'.$id.'</th>
+                            <td>'.$name.'</td>
+                            <td>'.$task.'</td>
+                            <td>'.$fieldid.'</td>
+                            </tr>';
+                        }
+                    }
+                ?>
+            </tbody>
+    </table>
+<?php } ?>
+
+
+<?php
 if(isset($_POST['id']) && $_POST['id'] === 'field201') {
     $sql = "call Get_Linemen(2);";
     $result = mysqli_query($conn, $sql);
