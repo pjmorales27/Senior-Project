6d57d72ec64d56dde3d5c722825b8bb59a382a9c
PJ Morales <105305127+pjmorales27@users.noreply.github.com>
2023-04-14 13:04:12 -0400

    Add files via upload

--- myjava.js
+++ myjava.js
@@ -1,11 +1,12 @@
-const dropdowns = document.querySelectorAll('dropdown');
-//loop through all dopdown elements
+const dropdowns = document.querySelectorAll('.dropdown');
+
+//loop through all dropdown elements
 dropdowns.forEach(dropdown => {
   //get inner elements from each dropdown
   const select = dropdown.querySelector('.select');
   const caret = dropdown.querySelector('.caret');
   const menu = dropdown.querySelector('.menu');
-  const options = dropdown.querySelector('.menu li');
+  const options = dropdown.querySelectorAll('.menu li');
   const selected = dropdown.querySelector('.selected');
   /*
     we are using this method in order to have 
@@ -14,10 +15,13 @@ dropdowns.forEach(dropdown => {
 
   //add click event to select element
   select.addEventListener('click', () => {
+
     //add the click select style to the select element
     select.classList.toggle('select-clicked');
+
     //add the rotate style to the caret element
     caret.classList.toggle('caret-rotate');
+
     //add the open styles to the menu element
     menu.classList.toggle('menu-open');
   });
@@ -27,7 +31,7 @@ dropdowns.forEach(dropdown => {
     //all a click event to the option element
     option.addEventListener('click', () => {
       //change selected inner text to the clicked option inner text 
-      selected.innerText = option.innerText;
+      selected.innerText = option.innerText
 
       //add the click style to the select element
       selected.classList.remove('select-clicked');
@@ -36,12 +40,13 @@ dropdowns.forEach(dropdown => {
       caret.classList.remove('caret-rotate');
 
       //add open style to the menu element
-      menu.classList.remove('menu-option');
+      menu.classList.remove('menu-open');
 
       //remove active class from all option elements
-      options.forEach(option => {
+      options.forEach(options => {
         option.classList.remove('active');
       });
+
       //add active class to clicked option element
       option.classList.add('active');
     });
